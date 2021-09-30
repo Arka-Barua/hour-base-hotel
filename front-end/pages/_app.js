afabcb1ba6169f4e3295c2cf91386a084ec751fd
Arka-dev <40537823+Arka-Barua@users.noreply.github.com>
2021-09-30 12:54:18 +0600

    Layouts setup added to _app.js

--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import AdminLayout from "../components/AdminLayout";
+import SignLayout from "../components/SignLayout";
+import UserLayout from "../components/UserLayout";
+import "../styles/globals.css";
+const layouts = {
+  admin: AdminLayout,
+  user: UserLayout,
+  sign: SignLayout,
+};
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  const Layout = layouts[Component.layout] || ((children) => <>{children}</>);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
 
-export default MyApp
+export default MyApp;
