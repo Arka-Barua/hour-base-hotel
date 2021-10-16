c20f1b952056302005e0ea31b24c6e71133fa152
Arka-dev <40537823+Arka-Barua@users.noreply.github.com>
2021-10-16 17:15:55 +0600

    Global prefix added

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,12 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
-      forbidNonWhitelisted: true,
     }),
   );
   await app.listen(4000);
