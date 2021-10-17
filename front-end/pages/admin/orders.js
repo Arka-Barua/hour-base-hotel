0bb286cd34bfa7136d8513ecbb6e5806b304613f
Arka-dev <40537823+Arka-Barua@users.noreply.github.com>
2021-10-17 12:38:38 +0600

    Room and Order page created

---
+++ front-end/pages/admin/orders.js
@@ -0,0 +1,11 @@
+import AdminLayout from "../../components/AdminLayout";
+
+const Orders = () => {
+  return (
+    <AdminLayout>
+      <main>Admin Orders</main>
+    </AdminLayout>
+  );
+};
+
+export default Orders;
---
+++ front-end/pages/admin/rooms.js
@@ -0,0 +1,10 @@
+import AdminLayout from "../../components/AdminLayout";
+const Rooms = () => {
+  return (
+    <AdminLayout>
+      <main>Admin Rooms</main>
+    </AdminLayout>
+  );
+};
+
+export default Rooms;
