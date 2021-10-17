4a9be5a7f18b85908ac01afe0de47118290b8cb2
Arka-dev <40537823+Arka-Barua@users.noreply.github.com>
2021-10-17 12:37:59 +0600

    Signout functionality created

--- front-end/components/Header.js
+++ front-end/components/Header.js
@@ -7,19 +7,63 @@ import {
   Toolbar,
   Typography,
 } from "@mui/material";
+import { useAuthContext } from "../context/AuthContext";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const {
+    setIsAuthenticated,
+    isAuthenticated,
+    isAdmin,
+    user,
+    setUser,
+    setIsAdmin,
+    setIsLoaded,
+    isLoaded,
+  } = useAuthContext();
+
+  useEffect(() => {
+    if (isLoaded && isAuthenticated) setIsAuthenticated(true);
+  }, [isAuthenticated, isLoaded]);
+
+  const SignOut = () => {
+    if (typeof window !== "undefined") {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      localStorage.removeItem("role");
+      setUser(null);
+      setIsAdmin(false);
+      setIsAuthenticated(false);
+      setIsLoaded(true);
+    }
+  };
+
+  const menuItems = [
+    {
+      text: "SignIn",
+      path: "/signin",
+    },
+    {
+      text: "SignUp",
+      path: "/signup",
+    },
+  ];
+
   return (
-    <AppBar position="static" elevation="false">
+    <AppBar
+      position="fixed"
+      elevation={0}
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
       <Container maxWidth="lg">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link href="#">HH</Link>
+            <Link href="/">HH</Link>
           </Typography>
           <div>
             <List sx={{ display: "flex" }}>
               <ListItem>
-                <Link href="#">
+                <Link href="/">
                   <a>Home</a>
                 </Link>
               </ListItem>
@@ -33,21 +77,39 @@ const Header = () => {
                   <a>Room</a>
                 </Link>
               </ListItem>
-              <ListItem>
-                <Link href="#">
-                  <a>SignUp</a>
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#">
-                  <a>SignIn</a>
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link href="/admin/dashboard">
-                  <a>Admin</a>
-                </Link>
-              </ListItem>
+              {!isAuthenticated ? (
+                menuItems.map((item) => (
+                  <ListItem key={item.text}>
+                    <Link href={item.path}>
+                      <a>{item.text}</a>
+                    </Link>
+                  </ListItem>
+                ))
+              ) : (
+                <ListItem>
+                  <Link href="#">
+                    <a style={{ cursor: "pointer" }} onClick={SignOut}>
+                      SignOut
+                    </a>
+                  </Link>
+                </ListItem>
+              )}
+              {isAdmin ? (
+                <ListItem>
+                  <Link href="/admin">
+                    <a>Admin</a>
+                  </Link>
+                </ListItem>
+              ) : null}
+              {user ? (
+                <ListItem>
+                  <Link href="#">
+                    <a>{user.firstname}</a>
+                  </Link>
+                </ListItem>
+              ) : (
+                ""
+              )}
             </List>
           </div>
         </Toolbar>
