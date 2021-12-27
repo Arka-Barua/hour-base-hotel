b8bf867549ffa8b8faa76b0c2d18b7dda351c3e1
Arka-dev <40537823+Arka-Barua@users.noreply.github.com>
2021-12-28 05:46:22 +0600

    BookingModule, UserModule added to Category Entity

--- back-end/src/category/category.module.ts
+++ back-end/src/category/category.module.ts
@@ -1,3 +1,4 @@
+import { BookingModule } from './../booking/booking.module';
 import { RoomModule } from './../room/room.module';
 import { Module, forwardRef } from '@nestjs/common';
 import { CategoryService } from './category.service';
@@ -5,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryController } from './category.controller';
 import { CategoryEntity } from './category.entity';
 import { MulterModule } from '@nestjs/platform-express';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
@@ -13,6 +15,8 @@ import { MulterModule } from '@nestjs/platform-express';
       dest: './upload',
     }),
     forwardRef(() => RoomModule),
+    forwardRef(() => BookingModule),
+    forwardRef(() => UserModule),
   ],
   providers: [CategoryService],
   controllers: [CategoryController],
