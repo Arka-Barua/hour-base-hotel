b3ae8d6fccc42c9d3717c172deb53ce1aec873e5
Arka-dev <40537823+Arka-Barua@users.noreply.github.com>
2021-12-28 05:34:11 +0600

    Category, Room, User Module added to BookingModule

--- back-end/src/booking/booking.module.ts
+++ back-end/src/booking/booking.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { RoomModule } from './../room/room.module';
+import { CategoryModule } from './../category/category.module';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookingController } from './booking.controller';
 import { BookingEntity } from './booking.entity';
 import { BookingService } from './booking.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BookingEntity])],
+  imports: [
+    TypeOrmModule.forFeature([BookingEntity]),
+    forwardRef(() => CategoryModule),
+    forwardRef(() => RoomModule),
+    forwardRef(() => UserModule),
+  ],
   providers: [BookingService],
   controllers: [BookingController],
+  exports: [BookingService],
 })
 export class BookingModule {}
