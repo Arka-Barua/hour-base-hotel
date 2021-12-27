969fbe651da525d04cc7ca04680af828ad844e8b
Arka-dev <40537823+Arka-Barua@users.noreply.github.com>
2021-12-28 05:28:26 +0600

    booking route list added to controller

--- back-end/src/booking/booking.controller.ts
+++ back-end/src/booking/booking.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { BookingService } from './booking.service';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Role } from 'src/user/user.role.enum';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 
 @Controller('booking')
-export class BookingController {}
+export class BookingController {
+  constructor(private bookingService: BookingService) {}
+
+  @Auth(Role.USER)
+  @Post('/create')
+  async createBooking(
+    @Body() body: any,
+    @CurrentUser() user: any,
+  ): Promise<any> {
+    return this.bookingService.createBooking(body, user);
+  }
+
+  @Auth(Role.ADMIN)
+  @Get()
+  async getBookings(): Promise<any> {
+    return this.bookingService.getBookings();
+  }
+
+  @Auth(Role.USER)
+  @Get('/getbookings/user')
+  async getUserBookings(@CurrentUser() user: any) {
+    return this.bookingService.getUserBookings(user);
+  }
+
+  @Auth(Role.ADMIN)
+  @Get('/getSinglebooking/:id')
+  async getSingleBooking(@Param() { id }): Promise<any> {
+    return this.bookingService.getSingleBooking(id);
+  }
+
+  @Auth(Role.ADMIN)
+  @Post('/assignRoom')
+  async assignRoom(@Body() body: any): Promise<any> {
+    return this.bookingService.assignRoom(body);
+  }
+
+  @Auth(Role.ADMIN)
+  @Get('/roomSearch/:id')
+  async roomSearch(@Param() { id }): Promise<any> {
+    return this.bookingService.roomSearch(id);
+  }
+}
