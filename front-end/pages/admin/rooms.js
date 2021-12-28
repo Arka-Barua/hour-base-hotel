05fbbba531ae27e8e4d3c4124524072098f30392
Arka-dev <40537823+Arka-Barua@users.noreply.github.com>
2021-12-28 06:33:48 +0600

    Extra space removed

--- front-end/pages/admin/rooms.js
+++ front-end/pages/admin/rooms.js
@@ -126,7 +126,6 @@ const Rooms = () => {
         setOpenPopup={setOpenPopup}
         title="Add New Room"
         draggable
-        // TransitionComponent={Transition}
         fullScreen
       >
         <Box>
